1356dd6938c1466d0f2157f70ee909dadd96a97b
Vasyl Merynher <vasyl_merynher@epam.com>
2024-11-29 18:23:08 +0200

    create domain models

---
+++ src/Booka/Booka.Domain/Models/Booking.cs
@@ -0,0 +1,12 @@
+﻿namespace Booka.Domain.Models;
+
+public class Booking : BaseEntity<int>
+{
+    public User User { get; set; }
+
+    public Workplace Workplace { get; set; }
+
+    public DateTime BookDate { get; set; }
+
+    public DateTime CheckInDate { get; set; }
+}
