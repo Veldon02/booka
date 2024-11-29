26135598239d3f4124ee108073a1c698b4bb0ff3
Vasyl Merynher <vasyl_merynher@epam.com>
2024-11-29 18:23:08 +0200

    create domain models

---
+++ src/Booka/Booka.Domain/Models/Workspace.cs
@@ -0,0 +1,14 @@
+﻿namespace Booka.Domain.Models;
+
+public class Workspace : BaseEntity<int>
+{
+    public string Name { get; set; }
+
+    public string Address { get; set; }
+
+    public string ContactEmail { get; set; }
+
+    public string ContactPhoneNumber { get; set; }
+
+    public IList<Workplace> Workplaces { get; set; }
+}
