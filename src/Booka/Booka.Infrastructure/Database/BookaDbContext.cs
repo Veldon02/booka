743663e663b44562f1fbada215a76667805d4034
Vasyl Merynher <vasyl_merynher@epam.com>
2024-12-01 19:27:47 +0200

    add dbcontext and missing field

---
+++ src/Booka/Booka.Infrastructure/Database/BookaDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Booka.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Booka.Infrastructure.Database;
+
+public class BookaDbContext : DbContext
+{
+    protected BookaDbContext(DbContextOptions<BookaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    public DbSet<Workspace> Workspaces { get; set; }
+
+    public DbSet<Workplace> Workplaces { get; set; }
+
+    public DbSet<Booking> Bookings { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(BookaDbContext).Assembly);
+    }
+}
