fc83b9da6998ca40bd39aee2b1fd67cc03ebb6e0
Vasyl Merynher <vasyl_merynher@epam.com>
2024-12-01 19:27:47 +0200

    add dbcontext and missing field

--- src/Booka/Booka.Domain/Models/User.cs
+++ src/Booka/Booka.Domain/Models/User.cs
@@ -8,6 +8,8 @@ public class User : BaseEntity<int>
 
     public string Email { get; set; }
 
+    public string PhoneNumber { get; set; }
+
     public string Password { get; set; }
 
     public List<Booking> Bookings { get; set; }
---
+++ src/Booka/Booka.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Booka.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Booka.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<BookaDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("Database"));
+        });
+    }
+}
