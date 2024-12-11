7b10c090232798d43f98ff1ea6edacedea61214a
Merynher, Vasyl (contracted) <vmerynhe@PMINTL.NET>
2024-12-11 11:47:41 +0200

    add workspace crud endpoints

---
+++ src/Booka/Booka.Domain/Interfaces/Services/IWorkspaceService.cs
@@ -0,0 +1,14 @@
+﻿using Booka.Domain.Models;
+
+namespace Booka.Domain.Interfaces.Services;
+
+public  interface IWorkspaceService
+{
+    Task<Workspace> AddAsync(Workspace workspace);
+
+    Task<List<Workspace>> GetAllAsync();
+
+    Task UpdateAsync(int workspaceId, Workspace workspace);
+
+    Task<Workspace> GetByIdAsync(int workspaceId);
+}
