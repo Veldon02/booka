7b15d97b1983d59a54744087c65ed0a5d33bb78d
Merynher, Vasyl (contracted) <vmerynhe@PMINTL.NET>
2025-01-27 17:03:01 +0200

    fix unique indexes;

--- src/Booka/Booka.Infrastructure/Database/Migrations/20250127150056_FixUniqueIndexes.Designer.cs
+++ src/Booka/Booka.Infrastructure/Database/Migrations/20250127150056_FixUniqueIndexes.Designer.cs
@@ -4,6 +4,7 @@ using Booka.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Booka.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(BookaDbContext))]
-    partial class BookaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250127150056_FixUniqueIndexes")]
+    partial class FixUniqueIndexes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
